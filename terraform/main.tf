4342e2ba50a0028e395fcf197fca26b159223714
Tanesh Badnore <98690981+FONZO1008@users.noreply.github.com>
2026-05-08 21:00:39 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "app_sg" {
 
 resource "aws_instance" "app_server" {
   ami                    = "ami-03f4878755434977f"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = "devops-key"
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
